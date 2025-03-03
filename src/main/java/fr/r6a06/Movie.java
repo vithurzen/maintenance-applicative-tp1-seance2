d1a1687e386ba982b690e4b77339d1dee12ed109
bl <bouchkhaf@gmail.com>
2025-03-03 15:40:15 +0100

    step8

--- src/main/java/fr/r6a06/Movie.java
+++ src/main/java/fr/r6a06/Movie.java
@@ -41,4 +41,12 @@ public class Movie {
         }
         return result;
     }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
+                daysRented > 1)
+            return 2;
+        else
+            return 1;
+    }
 }
--- src/main/java/fr/r6a06/Rental.java
+++ src/main/java/fr/r6a06/Rental.java
@@ -14,12 +14,7 @@ public class Rental {
     }
 
     public int getFrequentRenterPoints() {
-
-        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                this.getDaysRented() > 1)
-            return 2;
-        else
-            return 1;
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 
     public int getDaysRented() {
